249af4fdb9acd884c0e831514fc8d322125d225c
Feiling Xie <xie00083@algonquinlive.com>
2024-02-14 17:27:53 -0500

    the new master

--- app/src/main/java/data/MainViewModel.java
+++
@@ -1,9 +0,0 @@
-package data;
-import androidx.lifecycle.MutableLiveData;
-import androidx.lifecycle.ViewModel;
-public class MainViewModel extends ViewModel
-{
-    public MutableLiveData<String> editString=new MutableLiveData<>();
-    public MutableLiveData<Boolean> isSelected=new MutableLiveData<>();
-
-}
