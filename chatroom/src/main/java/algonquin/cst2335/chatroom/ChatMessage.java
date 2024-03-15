66bd5da09cba65ac8b76a367ff3bf23b02b49927
Feiling Xie <xie00083@algonquinlive.com>
2024-03-15 08:22:36 -0400

    week 7  started; add database part 1

--- chatroom/src/main/java/algonquin/cst2335/chatroom/ChatMessage.java
+++ chatroom/src/main/java/algonquin/cst2335/chatroom/ChatMessage.java
@@ -1,10 +1,22 @@
 package algonquin.cst2335.chatroom;
 
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
 public class ChatMessage
 {
+    @ColumnInfo(name="message")
     String message;
+    @ColumnInfo(name="timeSent")
     String timeSent;
+    @ColumnInfo(name="isSent")
     boolean isSentButton;
+    @PrimaryKey(autoGenerate=true)
+    @ColumnInfo(name="id")
+    public int id;
 
     ChatMessage(String m, String t, boolean sent)
     {
